4aa62525ce73b9e4f14666a38a3deafe019ac403
kotorabe <kotorajao@gmail.com>
2023-01-20 11:37:22 +0300

    Update UserAdminController.java

--- src/main/java/com/example/enchere/ControllerAdmin/UserAdminController.java
+++ src/main/java/com/example/enchere/ControllerAdmin/UserAdminController.java
@@ -2,27 +2,23 @@ package com.example.enchere.ControllerAdmin;
 
 import com.example.enchere.ModelAdmin.Categorie;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.enchere.ModelAdmin.Admin_User;
 
 import java.util.ArrayList;
 
-@RestController
+@Controller
 @CrossOrigin
 @RequestMapping("/Admin")
 public class UserAdminController {
-	/*@PostMapping("/LoginAdmin")
-	public Admin_User login(@RequestBody Admin_User user) throws Exception
+	@GetMapping
+	public String login() throws Exception
 	{
-		Admin_User login = new Admin_User().login(user);
-		return login;
-	}*/
+		return "index";
+	}
 	@PostMapping("/LoginAdmin")
 	public String login(HttpServletRequest request, Admin_User user, Model ModelAdmin) throws Exception
 	{
