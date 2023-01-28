abe746d2a866f8c25b278d80efba413a1dd541a7
kotorabe <kotorajao@gmail.com>
2023-01-28 12:12:01 +0300

    Update Connexion.java

--- src/main/java/com/example/enchere/Base/Connexion.java
+++ src/main/java/com/example/enchere/Base/Connexion.java
@@ -4,12 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Connexion {
-//	private static String url = "jdbc:postgresql://containers-us-west-185.railway.app:5889/railway";
-//    private static String user="postgres";
-//    private static String passwd = "NxPX0hpksmxrlYaTnqg5";
-    private static String url = "jdbc:postgresql://localhost:5432/enchere";
-    private static String user="enchere";
-    private static String passwd = "enchere";
+	private static String url = "jdbc:postgresql://containers-us-west-185.railway.app:5889/railway";
+    private static String user="postgres";
+    private static String passwd = "NxPX0hpksmxrlYaTnqg5";
+//    private static String url = "jdbc:postgresql://localhost:5432/enchere";
+//    private static String user="enchere";
+//    private static String passwd = "enchere";
 	private  Connection connect;
 	public  Connection setConnect() throws Exception
 	{
